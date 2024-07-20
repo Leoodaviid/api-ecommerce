1f0b72c2a0b652e61e1dc732f9c867efff147fd2
Leoodaviid <david_ax7@hotmail.com>
2024-07-19 22:40:59 -0300

    first commit

--- api/server.js
+++ api/server.js
@@ -1,19 +1,32 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const path = require("path");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+const routes = require("./routes.json");
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+// Apply default middlewares (logger, static, cors, and no-cache)
+server.use(middlewares);
+
+// Middleware to block routes that don't start with /api/v1/
+server.use((req, res, next) => {
+  if (!req.url.startsWith("/api/v1/")) {
+    res.sendStatus(404);
+  } else {
+    next();
+  }
+});
+
+// Rewriter to map custom routes from routes.json
+server.use(jsonServer.rewriter(routes));
+
+// Use default router
+server.use(router);
+
+// Start the server
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running ~ 🚀");
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
